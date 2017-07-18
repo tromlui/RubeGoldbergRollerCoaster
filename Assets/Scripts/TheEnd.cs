4a7a9ba66dda323937991787bcb30513eb4348ec
Unknown <sgranoff@gm.slc.edu>
2017-07-18 09:27:01 -0400

    Ready to build (I hope)

---
+++ Assets/Scripts/TheEnd.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheEnd : MonoBehaviour {
+
+	public GameObject endText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider Other) {
+		if (Other.tag=="ending") {
+			endText.SetActive (true);
+		}
+	}
+}
---
+++ Assets/Scripts/TheStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheStart : MonoBehaviour {
+
+	public GameObject startText;
+	public GameObject startDomino1;
+	public GameObject startDomino2;
+	public GameObject startingBlock;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			startText.SetActive (false);
+			startDomino1.SetActive (true);
+			startDomino2.SetActive (true);
+			startingBlock.SetActive (false);
+		}
+	}
+		
+}
